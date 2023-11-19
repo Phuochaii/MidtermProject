bc413a0763c36de60a1a6b4f1b05c56c8f0dd960
PC <truongvanhao159@gmail.com>
2023-11-19 08:53:42 +0700

    config cor

--- midterm-project/back-end/src/main.ts
+++ midterm-project/back-end/src/main.ts
@@ -5,6 +5,12 @@ import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
